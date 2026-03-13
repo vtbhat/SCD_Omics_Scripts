2224c2ddf81d8f2015558fbc9331a77089ada234
Varsha Bhat <82274960+vtbhat@users.noreply.github.com>
2026-03-13 12:35:26 -0400

    Updated file paths

--- PainOmics-Analysis-CD45.R
+++ PainOmics-Analysis-CD45.R
@@ -50,9 +50,9 @@ GeneSymbolAnnot <- function(input_matrix, justver = F)
 
 ####Load HTSeq files using DESeq2's function
 ####WILL NOT USE DESeq2 for analysis
-directory <- ("C:/Users/varsh/Desktop/GT Research/PainOmics_2023/HTSeq_PainOmics")
-sampleTable <- read.csv("C:/Users/varsh/Desktop/GT Research/PainOmics_2023/metadata_CD45.csv", header=TRUE)
-#sampleTable <- read.csv("C:/Users/varsh/Desktop/GT Research/PainOmics_2023/metadata_CD71.csv", header=TRUE)
+directory <- ("PainOmics_2023/HTSeq_PainOmics")
+sampleTable <- read.csv("PainOmics_2023/metadata_CD45.csv", header=TRUE)
+#sampleTable <- read.csv("PainOmics_2023/metadata_CD71.csv", header=TRUE)
 sampleTable <- na.omit(sampleTable)
 sampleTable <- subset(sampleTable, sampleTable$Timepoint!="")
 #sampleTable$sampleName <- paste0(sampleTable$CD71_NWGC_ID, ".txt")
@@ -191,7 +191,7 @@ res <- topTable(fit2, coef=2, genelist=fit$genes,number=Inf, adjust="BH")
 ####Annotate with gene symbols
 #Remove version number
 res <- GeneSymbolAnnot(res, justver = T)
-#write.csv(res_x, "C:/Users/varsh/Desktop/GT Research/PainOmics_2023/CD71_DEGs_VOCvsSS_ChronicPain.csv", row.names=FALSE)
+#write.csv(res_x, "PainOmics_2023/CD71_DEGs_VOCvsSS_ChronicPain.csv", row.names=FALSE)
 
 ####Enrichment analysis
 resSig <- res
@@ -215,7 +215,7 @@ fgseaResTidy <- fgseaRes %>%
 #Plotting it
 fgseaResTidy$pathway <- gsub("^.{0,9}", "", fgseaResTidy$pathway)
 fgseaResTidy$pathway <- gsub("_", " ", fgseaResTidy$pathway)
-#pdf(file="C:/Users/varsh/Desktop/GT Research/PainOmics_2023/ASH_2023_Poster/Fig1.pdf", width = 12)
+#pdf(file="PainOmics_2023/ASH_2023_Poster/Fig1.pdf", width = 12)
 ggplot(fgseaResTidy, aes(reorder(pathway, NES), NES)) +
   geom_col(aes(fill=padj<0.05)) +
   coord_flip() +
@@ -307,7 +307,7 @@ library(dplyr)
 ss <- sampleTablehmm %>% distinct(Unique.ID, .keep_all=TRUE)
 
 
-larger <- read.csv("C:/Users/varsh/Desktop/GT Research/PainOmics_2023/painomics_updated_metadata.csv", header=TRUE)
+larger <- read.csv("PainOmics_2023/painomics_updated_metadata.csv", header=TRUE)
 larger <- subset(larger, select=c("Date.of.collection_CD71", "CD71_NWGC_ID"))
 larger <- subset(larger, larger$CD71_NWGC_ID %in% sampleTable$CD71_NWGC_ID)
 
